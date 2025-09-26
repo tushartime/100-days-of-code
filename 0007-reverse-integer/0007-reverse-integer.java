418e64ab7a957bb8882f0442352afc0c65e2ae47
Tushar Sharma <timetusharindia@gmail.com>
2025-09-27 00:02:27 +0530

    Time: 2 ms (9.49%), Space: 41.7 MB (5.25%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    //tushar-first
+    public int reverse(int x) {
+        int res = 0;
+        boolean isNegative = x < 0;
+        String strX = String.valueOf(Math.abs(x));
+        StringBuilder sb = new StringBuilder(strX).reverse();
+        
+        try {
+            res = Integer.parseInt(sb.toString());
+        } catch (NumberFormatException e) {
+            return 0;
+        }
+        
+        return isNegative ? -res : res;       
+    }
+}
