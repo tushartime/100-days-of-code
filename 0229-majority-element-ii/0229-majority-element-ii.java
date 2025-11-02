c844b4b5c602b3f4f30561f0d89d1dc97821acf0
Tushar Sharma <timetusharindia@gmail.com>
2025-11-02 19:23:19 +0530

    Time: 4 ms (61.43%), Space: 52.4 MB (5.1%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        int count1 = 0, count2 = 0;
+        Integer candidate1 = null, candidate2 = null;
+
+        for (int num : nums) {
+            if (candidate1 != null && num == candidate1) {
+                count1++;
+            } else if (candidate2 != null && num == candidate2) {
+                count2++;
+            } else if (count1 == 0) {
+                candidate1 = num;
+                count1 = 1;
+            } else if (count2 == 0) {
+                candidate2 = num;
+                count2 = 1;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        // Second pass: verify actual counts
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (candidate1 != null && num == candidate1) count1++;
+            else if (candidate2 != null && num == candidate2) count2++;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        if (count1 > n / 3) result.add(candidate1);
+        if (count2 > n / 3) result.add(candidate2);
+
+        return result;
+    }
+}
