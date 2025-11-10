b8b8c802376ba002f90c2912a2d95272dde09558
Tushar Sharma <timetusharindia@gmail.com>
2025-11-10 19:38:24 +0530

    Time: 3 ms (82.93%), Space: 45.3 MB (90.1%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+        
+        // Copy last row into dp
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+        
+        // Bottom-up calculation
+        for (int row = n - 2; row >= 0; row--) {
+            for (int col = 0; col <= row; col++) {
+                dp[col] = triangle.get(row).get(col) + Math.min(dp[col], dp[col + 1]);
+            }
+        }
+        
+        return dp[0];
+    }
+}
+
